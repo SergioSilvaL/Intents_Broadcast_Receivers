c7958f0a96eff16c60372a93620a14b1ec27eb45
sergiosilva <sergio_silva@tecnologiasintech.com>
2017-08-15 16:55:57 -0700

    Intent Extras and Results

--- app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -19,9 +19,10 @@ import android.widget.Toast;
 import com.teamtreehouse.musicmachine.adapters.PlaylistAdapter;
 import com.teamtreehouse.musicmachine.models.Song;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    public static final String EXTRA_TITLE = "EXTRA_TITLE";
     public static final String KEY_SONG = "song";
 
     private boolean mBound = false;
@@ -100,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
     private void testIntents() {
         // Explicit intent
         Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_TITLE, "Gradle, Gradle, Gradle");
         startActivity(intent);
     }
 
